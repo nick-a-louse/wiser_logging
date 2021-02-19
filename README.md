7acb968843c8c67704623a61158faf52bc76f0e9
Nick Jefford-Horn <nick.jeffordhorn@gmail.com>
2021-02-19 21:55:18 +0000

    .

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# wiser_logging
-Logging data from the Drayton Wiser Heating Hub, storing it in AWS DynamoDB, and then graphing the results
+# Wiser
+
+This is a small personal project, to attempt to get to know Python and AWS DynamoDB better.
+
+The basic flow is:
+
+1) Store the date and time that the script is run (the intention is to graph the information with a maximum resolution of every 15 minutes, and this will be difficult if it isn't consistent)
+2) Iterate through all the rooms connected to the Wiser Hub and store in a dictionary (The hub automatically averages if there are multiple devices in a single room, i.e. two TRVs)
+3) Retrieve the local temperature and humidity from 'Open Weather Maps' and store in the same dictionary
+4) Upload the dictionary to AWS DynamoDB (using 'boto3').
+
+Pre-requisites:
+1) A Drayton Wiser heating system
+2) An 'Open Weather Maps' API key (free)
+
+Optional:
+1) A Google Cloud API key (you could just find your credentials and put them in the gmap.params config file)
---
+++ owm.py
@@ -0,0 +1,47 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+
+#-----------API keys-----------:
+
+with open("owm.params", "r") as f:
+    data = f.read().split("\n")
+
+owm_key = ""
+units = ""
+
+for lines in data:
+    line = lines.split("=")
+    if line[0] == "appid":
+        owm_key = line[1]
+    if line[0] == "units":
+        units = line[1]
+        
+
+
+#-----------API endpoint-----------
+owmserviceurl = "http://api.openweathermap.org/data/2.5/weather?"
+
+#-----------Building Open Weather Maps URL, and retrieving the JSON-----------
+
+def get_station (coordinates):
+
+    while True:
+        #-----------Setting up the parameters to create the concatenated url-----------
+        owm = dict()
+        owm['lat'] = coordinates[0]
+        owm['lon'] = coordinates[1]
+        owm['appid'] = owm_key
+        owm['units'] = units
+        owm_url = owmserviceurl + urllib.parse.urlencode(owm)
+
+        weather = urllib.request.urlopen(owm_url)
+        weather_raw = weather.read().decode()
+        
+        try:
+            weather_json = json.loads(weather_raw)
+        except:
+            print ('No station found')
+            break
+        
+        return (weather_json["main"]["temp"], weather_json["main"]["humidity"])
+        break
